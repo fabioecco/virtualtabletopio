f53ee9bfd8cb846752fe548f13954757ebd72798
Fabio Esteves <29529648+fabioecco@users.noreply.github.com>
2026-02-27 00:26:46 -0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,1 +1,9 @@
-
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyAGxmk1IZIxpi35hblWcl-YCDw_pKlOMYs",
+  authDomain: "virtualtabletopio.firebaseapp.com",
+  projectId: "virtualtabletopio",
+  storageBucket: "virtualtabletopio.firebasestorage.app",
+  messagingSenderId: "26907637871",
+  appId: "1:26907637871:web:2fdba2873a45b9a93cf59a"
+}
